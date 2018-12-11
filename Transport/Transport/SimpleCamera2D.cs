709cab5a1fbcfc07c3679fa5846930555888a586
Flexecute <tomsandeman@gmail.com>
2018-12-11 23:05:17 +1100

    Removed underscores.
    Modified transform to do the rotation first to ensure moving the view up and down is relative to the viewer, not the canvas.

--- Transport/Transport/SimpleCamera2D.cs
+++ Transport/Transport/SimpleCamera2D.cs
@@ -5,38 +5,38 @@ namespace Roboblob.XNA.WinRT.Camera
 {
     public class Camera2D
     {
-        private float _zoom;
-        private float _rotation;
-        private Vector2 _position;
-        private Matrix _transform = Matrix.Identity;
-        private bool _isViewTransformationDirty = true;
-        private Matrix _camTranslationMatrix = Matrix.Identity;
-        private Matrix _camRotationMatrix = Matrix.Identity;
-        private Matrix _camScaleMatrix = Matrix.Identity;
-        private Matrix _resTranslationMatrix = Matrix.Identity;
+        private float zoom;
+        private float rotation;
+        private Vector2 position;
+        private Matrix transform = Matrix.Identity;
+        private bool isViewTransformationDirty = true;
+        private Matrix camTranslationMatrix = Matrix.Identity;
+        private Matrix camRotationMatrix = Matrix.Identity;
+        private Matrix camScaleMatrix = Matrix.Identity;
+        private Matrix resTranslationMatrix = Matrix.Identity;
 
 
         protected ResolutionIndependentRenderer ResolutionIndependentRenderer;
-        private Vector3 _camTranslationVector = Vector3.Zero;
-        private Vector3 _camScaleVector = Vector3.Zero;
-        private Vector3 _resTranslationVector = Vector3.Zero;
+        private Vector3 camTranslationVector = Vector3.Zero;
+        private Vector3 camScaleVector = Vector3.Zero;
+        private Vector3 resTranslationVector = Vector3.Zero;
 
         public Camera2D(ResolutionIndependentRenderer resolutionIndependence)
         {
             ResolutionIndependentRenderer = resolutionIndependence;
 
-            _zoom = 0.1f;
-            _rotation = 0.0f;
-            _position = Vector2.Zero;
+            zoom = 0.1f;
+            rotation = 0.0f;
+            position = Vector2.Zero;
         }
 
         public Vector2 Position
         {
-            get { return _position; }
+            get { return position; }
             set
             {
-                _position = value;
-                _isViewTransformationDirty = true;
+                position = value;
+                isViewTransformationDirty = true;
             }
         }
 
@@ -52,15 +52,15 @@ namespace Roboblob.XNA.WinRT.Camera
 
         public float Zoom
         {
-            get { return _zoom; }
+            get { return zoom; }
             set
             {
-                _zoom = value;
-                if (_zoom < 0.1f)
+                zoom = value;
+                if (zoom < 0.1f)
                 {
-                    _zoom = 0.1f;
+                    zoom = 0.1f;
                 }
-                _isViewTransformationDirty = true;
+                isViewTransformationDirty = true;
             }
         }
 
@@ -68,52 +68,52 @@ namespace Roboblob.XNA.WinRT.Camera
         {
             get
             {
-                return _rotation;
+                return rotation;
             }
             set
             {
-                _rotation = value;
-                _isViewTransformationDirty = true;
+                rotation = value;
+                isViewTransformationDirty = true;
             }
         }
 
         public Matrix GetViewTransformationMatrix()
         {
-            if (_isViewTransformationDirty)
+            if (isViewTransformationDirty)
             {
-                _camTranslationVector.X = -_position.X;
-                _camTranslationVector.Y = -_position.Y;
+                camTranslationVector.X = -position.X;
+                camTranslationVector.Y = -position.Y;
 
-                Matrix.CreateTranslation(ref _camTranslationVector, out _camTranslationMatrix);
-                Matrix.CreateRotationZ(_rotation, out _camRotationMatrix);
+                Matrix.CreateTranslation(ref camTranslationVector, out camTranslationMatrix);
+                Matrix.CreateRotationZ(rotation, out camRotationMatrix);
 
-                _camScaleVector.X = _zoom;
-                _camScaleVector.Y = _zoom;
-                _camScaleVector.Z = 1;
+                camScaleVector.X = zoom;
+                camScaleVector.Y = zoom;
+                camScaleVector.Z = 1;
 
-                Matrix.CreateScale(ref _camScaleVector, out _camScaleMatrix);
+                Matrix.CreateScale(ref camScaleVector, out camScaleMatrix);
 
-                _resTranslationVector.X = ResolutionIndependentRenderer.VirtualWidth*0.5f;
-                _resTranslationVector.Y = ResolutionIndependentRenderer.VirtualHeight * 0.5f;
-                _resTranslationVector.Z = 0;
+                resTranslationVector.X = ResolutionIndependentRenderer.VirtualWidth*0.5f;
+                resTranslationVector.Y = ResolutionIndependentRenderer.VirtualHeight * 0.5f;
+                resTranslationVector.Z = 0;
 
-                Matrix.CreateTranslation(ref _resTranslationVector, out _resTranslationMatrix);
+                Matrix.CreateTranslation(ref resTranslationVector, out resTranslationMatrix);
 
-                _transform = _camTranslationMatrix *
-                             _camRotationMatrix *
-                             _camScaleMatrix *
-                             _resTranslationMatrix *
+                transform = camRotationMatrix*
+                              camTranslationMatrix *
+                             camScaleMatrix *
+                             resTranslationMatrix *
                              ResolutionIndependentRenderer.GetTransformationMatrix();
                 
-                _isViewTransformationDirty = false;
+                isViewTransformationDirty = false;
             }
 
-            return _transform;
+            return transform;
         }
 
         public void RecalculateTransformationMatrices()
         {
-            _isViewTransformationDirty = true;
+            isViewTransformationDirty = true;
         }
     }
 }
